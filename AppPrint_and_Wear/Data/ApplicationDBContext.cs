c336402cddcf6102a7845447687a93836062a823
Wilmer Pérez Hernández <wilmerperezh0312@gmail.com>
2025-09-28 23:17:40 -0600

    agregue el fllujo de la facturacion la relacion y tambien los controladores, y la correccion de la profe

--- AppPrint_and_Wear/Data/ApplicationDBContext.cs
+++ AppPrint_and_Wear/Data/ApplicationDBContext.cs
@@ -1,13 +1,16 @@
 ﻿using AppPrint_and_Wear.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
 namespace AppPrint_and_Wear.Data
 {
     public class ApplicationDBContext : DbContext
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
- : base(options)
+            : base(options)
         {
         }
+
         public DbSet<Administrador> Administradores { get; set; }
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Carrito_De_Compra> Carrito_De_Compras { get; set; }
@@ -15,12 +18,20 @@ namespace AppPrint_and_Wear.Data
         public DbSet<Producto> Productos { get; set; }
         public DbSet<Metodo_De_Pago> Metodo_De_Pagos { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
-
         public DbSet<Personalizacion> Personalizaciones { get; set; }
+        public DbSet<Factura> Facturas { get; set; }
+        public DbSet <DetalleFactura> DetalleFacturas { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
-
-
-
+            // Evita cascada automática en todas las relaciones
+            foreach (var foreignKey in modelBuilder.Model.GetEntityTypes()
+                         .SelectMany(e => e.GetForeignKeys()))
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
     }
 }
---
+++ AppPrint_and_Wear/Models/DetalleFactura.cs
@@ -0,0 +1,15 @@
+﻿namespace AppPrint_and_Wear.Models
+{
+    public class DetalleFactura
+    {
+        public int DetalleFacturaId { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public double PrecioUnitario { get; set; }
+        public double Subtotal { get; set; }
+        // Relaciones
+        public int FacturaId { get; set; } // Llave foránea
+        public Factura Factura { get; set; } // Propiedad de navegación
+        public int ProductoId { get; set; } // Llave foránea
+    }
+}

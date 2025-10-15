b233dc45480f2385d7a971e5819bca7ac597dc9e
unknown <wuendolinvalverde@gmail.com>
2025-10-15 16:00:26 -0600

    agregue mas camisas y colores

---
+++ AppPrint_and_Wear/Migrations/20251015030515_Primera.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppPrint_and_Wear.Migrations
+{
+    /// <inheritdoc />
+    public partial class Primera : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Administradores",
+                columns: table => new
+                {
+                    AdministradorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellidos = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Contraseña = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefono = table.Column<int>(type: "int", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Administradores", x => x.AdministradorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Carrito_De_Compra = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellidos = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Contraseña = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefono = table.Column<int>(type: "int", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.ClienteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descriccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Precio = table.Column<double>(type: "float", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Metodo_De_Pagos",
+                columns: table => new
+                {
+                    Metodo_De_PagoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Metodo_Tipo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Numero_Tarjeta = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CVV = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Metodo_De_Pagos", x => x.Metodo_De_PagoId);
+                    table.ForeignKey(
+                        name: "FK_Metodo_De_Pagos_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "ClienteId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carrito_De_Compras",
+                columns: table => new
+                {
+                    Carrito_De_CompraId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Total = table.Column<double>(type: "float", nullable: false),
+                    ClienteId = table.Column<int>(type: "int", nullable: false),
+                    Metodo_De_PagoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrito_De_Compras", x => x.Carrito_De_CompraId);
+                    table.ForeignKey(
+                        name: "FK_Carrito_De_Compras_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "ClienteId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Carrito_De_Compras_Metodo_De_Pagos_Metodo_De_PagoId",
+                        column: x => x.Metodo_De_PagoId,
+                        principalTable: "Metodo_De_Pagos",
+                        principalColumn: "Metodo_De_PagoId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartItems",
+                columns: table => new
+                {
+                    CartItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    SubTotal = table.Column<double>(type: "float", nullable: false),
+                    Carrito_De_Compra_Id = table.Column<int>(type: "int", nullable: false),
+                    Carrito_De_CompraId = table.Column<int>(type: "int", nullable: true),
+                    ProductoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartItems", x => x.CartItemId);
+                    table.ForeignKey(
+                        name: "FK_CartItems_Carrito_De_Compras_Carrito_De_CompraId",
+                        column: x => x.Carrito_De_CompraId,
+                        principalTable: "Carrito_De_Compras",
+                        principalColumn: "Carrito_De_CompraId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_CartItems_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "ProductoId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Envios",
+                columns: table => new
+                {
+                    EnvioId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Direccion_De_Envio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ShippingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Tiempo_Estimado = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Numero_De_Rastreo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Carrito_De_CompraId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Envios", x => x.EnvioId);
+                    table.ForeignKey(
+                        name: "FK_Envios_Carrito_De_Compras_Carrito_De_CompraId",
+                        column: x => x.Carrito_De_CompraId,
+                        principalTable: "Carrito_De_Compras",
+                        principalColumn: "Carrito_De_CompraId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Facturas",
+                columns: table => new
+                {
+                    FacturaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Total = table.Column<double>(type: "float", nullable: false),
+                    Carrito_De_CompraId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Facturas", x => x.FacturaId);
+                    table.ForeignKey(
+                        name: "FK_Facturas_Carrito_De_Compras_Carrito_De_CompraId",
+                        column: x => x.Carrito_De_CompraId,
+                        principalTable: "Carrito_De_Compras",
+                        principalColumn: "Carrito_De_CompraId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetalleFacturas",
+                columns: table => new
+                {
+                    DetalleFacturaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    PrecioUnitario = table.Column<double>(type: "float", nullable: false),
+                    Subtotal = table.Column<double>(type: "float", nullable: false),
+                    FacturaId = table.Column<int>(type: "int", nullable: false),
+                    ProductoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetalleFacturas", x => x.DetalleFacturaId);
+                    table.ForeignKey(
+                        name: "FK_DetalleFacturas_Facturas_FacturaId",
+                        column: x => x.FacturaId,
+                        principalTable: "Facturas",
+                        principalColumn: "FacturaId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carrito_De_Compras_ClienteId",
+                table: "Carrito_De_Compras",
+                column: "ClienteId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carrito_De_Compras_Metodo_De_PagoId",
+                table: "Carrito_De_Compras",
+                column: "Metodo_De_PagoId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_Carrito_De_CompraId",
+                table: "CartItems",
+                column: "Carrito_De_CompraId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_ProductoId",
+                table: "CartItems",
+                column: "ProductoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleFacturas_FacturaId",
+                table: "DetalleFacturas",
+                column: "FacturaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Envios_Carrito_De_CompraId",
+                table: "Envios",
+                column: "Carrito_De_CompraId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Facturas_Carrito_De_CompraId",
+                table: "Facturas",
+                column: "Carrito_De_CompraId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Metodo_De_Pagos_ClienteId",
+                table: "Metodo_De_Pagos",
+                column: "ClienteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Administradores");
+
+            migrationBuilder.DropTable(
+                name: "CartItems");
+
+            migrationBuilder.DropTable(
+                name: "DetalleFacturas");
+
+            migrationBuilder.DropTable(
+                name: "Envios");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "Facturas");
+
+            migrationBuilder.DropTable(
+                name: "Carrito_De_Compras");
+
+            migrationBuilder.DropTable(
+                name: "Metodo_De_Pagos");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
